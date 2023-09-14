40442d2bc885cf260c8fb1880b94fb896aa0825d
JoaoVLima <celu.joaolima@gmail.com>
2023-09-14 12:58:33 -0300

    Debugging fetch

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 const http = require('http');
 const process = require('process');
+const path = require('node:path');
 
 let hostname = '127.0.0.1';
 let port = 3000;
+let template = 'github-style';
 
 function usage() {
     console.log(`Usage: npm run server [OPTIONS]`);
     console.log('Options:');
     console.log(' -h,  --help                Display this help message');
-    console.log(' -hn, --hostname [HOSTNAME] Specify hostname, default: 127.0.0.1');
-    console.log(' -p,  --port [PORT]         Specify port, default: 3000');
+    console.log(' -hn, --hostname [HOSTNAME] Specify hostname           default: 127.0.0.1');
+    console.log(' -p,  --port [PORT]         Specify port               default: 3000');
+    console.log(' -t,  --template [TEMPLATE] Specify template           default: github-style');
 }
 
 function handle_options() {
@@ -19,18 +22,25 @@ function handle_options() {
         let arg = process.argv[i]
 
         let next_arg = null;
+        // Verify if there is a next_argument
         if(i < process.argv.length-1){
             next_arg = process.argv[i+1];
         }
         
-        if (['-h', '--help'].includes(arg)){
+        if(['-h', '--help'].includes(arg)){
             usage();
             status = false;
             break;
-        }else if(['-hn', '--hostname'].includes(arg)){
+        }
+
+        if(!next_arg) continue;
+
+        if(['-hn', '--hostname'].includes(arg)){
             hostname = next_arg;
         }else if(['-p', '--port'].includes(arg)){
             port = parseInt(next_arg);
+        }else if(['-t', '--template'].includes(arg)){
+            template = next_arg;
         }
     }
 
@@ -41,10 +51,22 @@ function main() {
     let status = handle_options();
     if (!status) return;
     
+    let index = 'nice';
+
+    fetch(path.resolve(__dirname, 'src/index.html'))
+         .then(result => result.text())
+         .then(data => {
+             index = data;
+         })
+        .catch(error => {console.error(error)});
+
+    console.log(index);
+
     const server = http.createServer((req, res) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World');
+        res.setHeader('Content-Length', Buffer.byteLength(index));
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end(index);
     });
 
     server.listen(port, hostname, () => {
@@ -55,4 +77,3 @@ function main() {
 if (require.main === module) {
     main();
 }
-
