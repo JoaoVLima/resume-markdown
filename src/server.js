76c7e2d5bcde54bfb6f69d34ef29e75da17ce651
JoaoVLima <celu.joaolima@gmail.com>
2023-09-14 11:44:38 -0300

    server file with script argv

--- src/server.js
+++ src/server.js
@@ -6,26 +6,40 @@ let port = 3000;
 
 function usage() {
     console.log(`Usage: npm run server [OPTIONS]`);
-    console.log("Options:");
-    console.log(" -h,  --help                Display this help message");
-    console.log(" -hn, --hostname [HOSTNAME] Specify hostname");
-    console.log(" -p,  --port [PORT]         Specify port");
+    console.log('Options:');
+    console.log(' -h,  --help                Display this help message');
+    console.log(' -hn, --hostname [HOSTNAME] Specify hostname, default: 127.0.0.1');
+    console.log(' -p,  --port [PORT]         Specify port, default: 3000');
 }
 
 function handle_options() {
+    let status = true;
+
     for(var i = 0; i < process.argv.length; ++i) {
-        console.log(`${i}: ${process.argv[i]}`);
+        let arg = process.argv[i]
+
         let next_arg = null;
         if(i < process.argv.length-1){
             next_arg = process.argv[i+1];
         }
         
-        
+        if (['-h', '--help'].includes(arg)){
+            usage();
+            status = false;
+            break;
+        }else if(['-hn', '--hostname'].includes(arg)){
+            hostname = next_arg;
+        }else if(['-p', '--port'].includes(arg)){
+            port = parseInt(next_arg);
+        }
     }
+
+    return status;
 }
 
 function main() {
-    console.log("Hello world");
+    let status = handle_options();
+    if (!status) return;
     
     const server = http.createServer((req, res) => {
         res.statusCode = 200;
